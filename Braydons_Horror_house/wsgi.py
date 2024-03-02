37b65ebbaec87b2475d0f9aabb1bceb12aa79ee6
Manjula Lal <manjula_lal@hotmail.com>
2024-03-02 16:00:59 +0000

    initial commit

---
+++ Braydons_Horror_house/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Braydons_Horror_house project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Braydons_Horror_house.settings')
+
+application = get_wsgi_application()
