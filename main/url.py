c7cb9f1417d145bc41a2c629e8c6c31dcac0620c
Manjula Lal <manjula_lal@hotmail.com>
2024-03-11 17:13:27 +0000

    added models and forms

--- main/url.py
+++ main/url.py
@@ -2,7 +2,8 @@ from . import views
 from django.urls import path, include
 urlpatterns = [
     path('', views.ArticleList.as_view(), name='home'),
-    path("about/", include("about.urls"), name="about-urls"),    
+    path("shop/", include("shop.urls"), name="shop-urls"), 
+     path("stories/", include("stories.urls"), name="stories-urls"),     
     path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
     path('events/', views.EventList.as_view(), name='events'),
     path('events/<slug:slug>/', views.event_detail, name='event_detail'),
--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import About
 
-# Create your views here.
+
+class AboutList(ListView):
+    """
+    Renders a list of About pages
+    """
+    queryset = About.objects.all()
+    model = About  # Specify the model to work with
+    template_name = "about/about.html"  # Specify the template to render
+    context_object_name = "about_pages"  # Specify the context object name to use in the template
+    #html_content = model.mydiary
