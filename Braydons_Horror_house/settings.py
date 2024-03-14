8cc44fd96d181cd2f06d2040459af63ee5fd71ec
Manjula Lal <manjula_lal@hotmail.com>
2024-03-14 15:00:06 +0000

    added POST comment

--- Braydons_Horror_house/settings.py
+++ Braydons_Horror_house/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django_summernote',
     'allauth',
     'allauth.account',
@@ -63,6 +65,9 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/stories/'
 LOGOUT_REDIRECT_URL = '/stories/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
