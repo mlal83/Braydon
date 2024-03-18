9e85276c6df565894abf8406111ad5f0ee8273de
Manjula Lal <manjula_lal@hotmail.com>
2024-03-18 10:13:47 +0000

    added styling to register

--- stories/urls.py
+++ stories/urls.py
@@ -7,13 +7,14 @@ from .views import StoryDetailView
 urlpatterns = [
     path('', views.story_list, name='home'),  
     path('profile/', views.profile_view, name='profile'),
+    path('', views.story_list, name='home'),
+    path('profile_picture_upload/', views.profile_picture_upload, name='profile_picture_upload'),
     path('set_avatar/', views.set_avatar, name='set_avatar'),
     path('accounts/', include('allauth.urls')),
     path('story/<slug:slug>/', StoryDetailView.as_view(), name='stories_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>/', views.comment_delete, name='comment_delete'),
     path('submit-story/', views.submit_story, name='submit_story'),
-    path('upload-profile-picture/', views.profile_picture_upload, name='profile_picture_upload'),
     path('logout/', auth_views.LogoutView.as_view(), name='account_logout'),
     
 ]
