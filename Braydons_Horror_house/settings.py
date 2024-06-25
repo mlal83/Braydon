e9895faf64d214a135b46a2df23db58411ae30fd
Manjula Lal <manjula_lal@hotmail.com>
2024-06-25 19:28:57 +0000

    test8

--- Braydons_Horror_house/settings.py
+++ Braydons_Horror_house/settings.py
@@ -152,8 +152,14 @@ cloudinary.config(
     api_key=os.environ.get('CLOUDINARY_API_KEY'),
     api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
     secure=True,
-)
-
+    )
+ 
+#CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
+#os.environ['SECRET_KEY'] = 'your_secret_key_here'
+#os.environ['DATABASE_URL'] = 'your_database_url_here'
+#os.environ['CLOUDINARY_CLOUD_NAME'] = 'your_cloudinary_cloud_name_here'
+#os.environ['CLOUDINARY_API_KEY'] = 'your_cloudinary_api_key_here'
+#os.environ['CLOUDINARY_API_SECRET'] = 'your_cloudinary_api_secret_here'
 # Media settings
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/media/'
