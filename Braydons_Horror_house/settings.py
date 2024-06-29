e9870b594cee51020518ee61afac3d55ccdded47
Manjula Lal <manjula_lal@hotmail.com>
2024-06-29 18:03:39 +0000

    testing cloudinairy url3

--- Braydons_Horror_house/settings.py
+++ Braydons_Horror_house/settings.py
@@ -160,13 +160,13 @@ cloudinary.config(
     secure=True,
 )
 
-load_dotenv()
+# load_dotenv()
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
-}
+# CLOUDINARY_STORAGE = {
+#     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+#     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+#     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
+# }
 
 
 # Media settings
