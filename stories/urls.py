3ed268801822a60d46366e77a9212e6485d26fcb
Manjula Lal <manjula_lal@hotmail.com>
2024-07-03 19:45:28 +0000

    fixed broken pictire links, and styling to improve User experience

--- stories/urls.py
+++ stories/urls.py
@@ -14,15 +14,12 @@ urlpatterns = [
     path('profile/<int:profile_id>/', views.view_profile, name='view_profile'),
     path('profile/', views.profile_view, name='profile'),
     path('accounts/', include('allauth.urls')),
-    
     #path('<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
-    
     path('<slug:slug>/delete_comment/<int:comment_id>/', views.comment_delete, name='comment_delete'),
     path('submit-story/', views.submit_story, name='submit_story'),
     path('logout/', auth_views.LogoutView.as_view(), name='account_logout'),
     #path('story/<int:story_id>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
     path('story/<slug:slug>/delete_comment/<int:comment_id>/', views.comment_delete, name='comment_delete'),
-   
     path('<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
 
 ]
